61f55556abcf3c86024b7ace8606280e45e60238
j-martinez-s <141205822+j-martinez-s@users.noreply.github.com>
2023-08-11 14:57:32 -0300

    Add files via upload

---
+++ js/validacion.js
@@ -0,0 +1,37 @@
+function validatePassword() {
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+    if (password1.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres.");
+        return false;
+    }
+    if (password1 !== password2) {
+        alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
+        return false;
+    }
+    return true;
+}
+
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+function validate() {
+    var name = document.getElementById("nombre").value;
+    var surename = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    
+    if (name && surename && email && validatePassword()) {
+         showAlertSuccess()
+    } else { showAlertError()}
+}
+
+var boton = document.getElementById("regBtn");
+
+boton.addEventListener("click", validate);  
+
+
